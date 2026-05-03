ec9ae268ed44e1d6c04c7bd58742f746705906ab
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:40:35 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// trademark-cost-calculator - Trademark cost calculator - Fee estimation for DPMA, EUIPO, WIPO filings. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
